cc02db5f8822bcba76a7b94cf8870b1f2edc1239
drklee3 <derrick@dlee.dev>
2019-05-01 09:43:59 -0700

    Remove unnecessary quicksort assignment

--- hw4/1/QuickSort.java
+++ hw4/1/QuickSort.java
@@ -5,10 +5,9 @@ public class QuickSort {
         int input[] = new int[] {500, 700, 800, 100, 300, 200, 900, 400, 1000, 600};
         System.out.print("Input: ");
         System.out.println(Arrays.toString(input));
-        QuickSorter q = new QuickSorter(input);
+        new QuickSorter(input);
         
         System.out.print("Output: ");
         System.out.println(Arrays.toString(input));
     }
-
 }
