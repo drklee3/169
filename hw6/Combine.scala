d8c2933b0c40b7217632410f4d5311aa429802f1
drklee3 <derrick@dlee.dev>
2019-05-16 15:16:05 -0700

    finish hw6 #3

--- hw6/Combine.scala
+++ hw6/Combine.scala
@@ -3,10 +3,17 @@ object Combine {
 
   def square(x: Int): Int = x * x;
 
+  /**
+   * #1 applies a function f to the integers from 1 to x, then combines those
+   * values using another function g.
+   */
   def applyCombine(f: Int => Int, x: Int, g: (Int, Int) => Int): Int =
     if (x == 1) f(x)
     else g(f(x), applyCombine(f, x - 1, g));
   
+  /**
+   * #2 longhand curried version of #1
+   */
   def applyCombine2(f: Int => Int): Int => (((Int, Int) => Int) => Int) =
     (x: Int) => {
       (g: (Int, Int) => Int) => {
@@ -15,13 +22,18 @@ object Combine {
       }
     };
   
+  /**
+   * shorthand curried version of #1 
+   */
   def applyCombine3(f: Int => Int)(x: Int)(g: (Int, Int) => Int): Int =
     if (x == 1) f(x)
     else g(f(x), applyCombine(f, x - 1, g));
 
   def main(args: Array[String]): Unit = {
     println(applyCombine(square, 4, add));
+
     println(applyCombine2(square)(4)(add));
+    
     println(applyCombine3(square)(4)(add));
   }
 }
