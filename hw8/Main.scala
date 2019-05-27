98a367e19d9b3d55bf41816a24ec7c7af04b3120
drklee3 <derrick@dlee.dev>
2019-05-27 16:28:25 -0700

    update hw8

--- hw8/Main.scala
+++ hw8/Main.scala
@@ -3,15 +3,20 @@ object Main {
      * [#1] mimics the behavior of a typical procedural loop.
      *
      * Most important shortcoming compared to a C++ for loop is:
-     * (not tail-call optimization)
-     * maybe the parameters aren't generic?
+     * the parameters are required and aren't generic (they must be
+     * Int => Boolean and Int => Int) making it more restrictive
+     *
+     * (not memory usage because of tail-call optimization, it won't run into a
+     * stack overflow or have unnecessary overhead as the compiler will optimize
+     * the recursive tail call into a while loop, preventing the need for stack
+     * usage)
      */
     def forLoop(
         i: Int,
         cond: Int => Boolean,
-        inc: Int => Int
+        inc: Int => Int,
     )(
-        body: => Unit
+        body: => Unit,
     ): Unit =
         if (cond(i)) {
             body;
@@ -34,28 +39,76 @@ object Main {
         x => contains(s1, x) && !contains(s2, x);
     
     /**
-     * [#2a]
+     * [#2a] recursive method of set consisting of all Ints in provided List
      */
     def setList(sets: List[Int]): Int => Boolean =
+        if (sets.isEmpty) x => false // base case, empty set
+        else union(singletonSet(sets.head), setList(sets.tail));
+
+    /**
+     * iterative method w/ map & reduce of #2a
+     */
+    def setListM(sets: List[Int]): Int => Boolean =
         sets.map(singletonSet(_))
-            .reduce((x, y) => union(x, y));
+            .reduce(union(_, _));
     
     /**
-     * [#2b]
+     * [#2b] Creates a set with elements of s that satisfy p
      */
     def filter(s: Int => Boolean, p: Int => Boolean): Int => Boolean =
         intersect(s, p);
     
     /**
-     * [#2c] 
+     * literally just the intersect body
      */
-    def forall(s: Int => Boolean, p: Int => Boolean): Boolean =
+    def filterR(s: Int => Boolean, p: Int => Boolean): Int => Boolean =
+        x => contains(s, x) && contains(p, x);
+    
+    /**
+     * [#2c] Recursive function to test if p is true for all elements of s
+     * 
+     * Short circuits when there is an element in s that is false for p
+     * instead of doing p(x) && apply(x + 1) which will check all values even if
+     * it finds a false
+     */
+    def forall(s: Int => Boolean, p: Int => Boolean): Boolean = {
+        def apply(x: Int): Boolean =
+            if (x > 1000) true                     // reached the end
+            else if (!contains(s, x)) apply(x + 1) // not in set, next value
+            else if (p(x)) apply(x + 1)            // in set, true  for p
+            else false;                            // in set, false for p
+
+        apply(-1000)
+    };
+
+    /**
+     * Iterative forall function
+     */
+    def forallI(s: Int => Boolean, p: Int => Boolean): Boolean =
         -1000.to(1000)
-             .exists(p);
+             .filter(s)
+             .forall(p);
+    
+    /**
+     * Prints the values in a set
+     */
+    def printSet(s: Int => Boolean): Unit =
+        println(-1000.to(1000)
+                     .filter(s)
+                     .mkString(", "));
 
     def main(args: Array[String]): Unit = {
         forLoop(10, x => x > 0, x => x - 1) {
             println("Hello World");
         };
+
+        val l1 = setList(List(1, 3, 5, 9, 11));
+        val l2 = setList(List(1, 3, 5, 9, 11, 12));
+
+        printSet(l1);
+        printSet(l2);
+
+        println(forall(l1, x => x % 2 == 1)); // true
+        println(forall(l2, x => x % 2 == 1)); // false
     }
 }
--- hw8/Set.scala
+++ hw8/Set.scala
@@ -3,27 +3,41 @@ class Set(f: Int => Boolean) {
         this.f(elem)
 
     /**
-     * Union
+     * Union of this and tc
      */
     def \/(t: Set): Set =
         new Set(x => this.contains(x) || t.contains(x));
 
     /**
-     * Intersection
+     * Intersection of this and t
      */
     def /\(t: Set): Set =
         new Set(x => this.contains(x) && t.contains(x));
 
     /**
-     * Difference
+     * Difference of this and t
      */
     def -(t: Set): Set =
         new Set(x => this.contains(x) && !t.contains(x));
 
+    /**
+     * New set of elements of s that satisfy p
+     */
     def filter(p: Int => Boolean): Set =
         new Set(x => this.contains(x) && p(x));
+    
+    def forall(p: Int => Boolean): Boolean = {
+        def apply(x: Int): Boolean =
+            if (x > 1000) true
+            else if (!this.contains(x)) apply(x + 1)
+            else if (p(x)) apply(x + 1)
+            else false;
 
-    def forall(p: Int => Boolean): Boolean =
+        apply(-1000)
+    };
+
+    def forallI(p: Int => Boolean): Boolean =
         -1000.to(1000)
+             .filter(f)
              .forall(p);
 }
